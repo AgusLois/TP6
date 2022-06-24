d2d7e733cd61cc74f99cb546b056635d5bd7b207
AgusLois <Agusslois98@gmail.com>
2022-06-24 11:48:54 -0300

    TP6-Lois

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,32 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
-  </div>
+  <section>
+    <div id="app">
+      <Home />
+    </div>
+  </section>
 </template>
 
+<script>
+
+import Home from "./componentes/Home.vue";
+
+export default {
+  name: 'App',
+  components: {
+    Home,
+  },
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+body {
+  background: #232323;
+  margin: 0;
+  font-family: "Montserrat", "Avenir";
 }
 </style>
